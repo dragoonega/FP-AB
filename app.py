5833b2817d8a1c649822b8a986646dd54a489db6
Ega Fernanda Putra <dragoon.fernanda12@gmail.com>
2022-12-13 15:12:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def diagnosis(file):
     ##YOUR CODE GOES HERE##
         plt.imshow(image)
         plt.savefig("diagpic.png")
-        st.image("diagpic.png" ,caption= "X- Ray Diagnosis Picture" , channels="RGB", width=500)
+        st.image("diagpic.png" ,caption= "X- Ray Diagnosis Picture" , channels="RGB", width=400)
     
 
     # Load model  
